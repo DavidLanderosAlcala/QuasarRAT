4f1136faa0fcdac96be33cdd69656a2062552b28
David Landeros <david.landeros@gameloft.com>
2017-06-27 15:35:38 -0700

    Add settings.js

---
+++ C&C Server/settings.js
@@ -0,0 +1,9 @@
+
+/** @global Settings
+  * @desc This object holds the global configuration of the server
+  */
+module.exports = {
+	adminPassword : "keep-the-secret",
+    adminPort     : 12345,
+    clientPort    : 12346,
+};
